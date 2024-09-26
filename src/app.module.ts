7f6c760501444af62ebdf14bc2f34fd845b26c41
Oleksandr Ptukha <souvlaki@Oleksandrs-MacBook-Air.local>
2024-09-26 15:26:53 +0300

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,12 +14,12 @@ import { Deal} from "./deals/deals.model";
         }),
         SequelizeModule.forRoot({
           dialect: 'postgres',
-          host: process.env.POSTGRES_HOST,
-          port: Number(process.env.POSTGRESS_PORT),
-          username: process.env.POSTGRES_USER,
-          password: process.env.POSTGRESS_PASSWORD,
-          uri: process.env.POSTGRESS_URL,
-          database: process.env.POSTGRES_DB,
+          host: process.env.POSTGRES_HOST || 'c3l5o0rb2a6o4l.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com', 
+          port: Number(process.env.POSTGRESS_PORT) || 5432,
+          username: process.env.POSTGRES_USER || 'ud3jgalol1nm1g',
+          password: process.env.POSTGRESS_PASSWORD || 'p6e3aef7e0b4ca176c908b66421930b96d6e9b00d3221651d8edfb13457b2af11',
+          uri: process.env.POSTGRESS_URL || 'postgres://ud3jgalol1nm1g:p6e3aef7e0b4ca176c908b66421930b96d6e9b00d3221651d8edfb13457b2af11@c3l5o0rb2a6o4l.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/dflr3ocd7b9j5e',
+          database: process.env.POSTGRES_DB || 'dflr3ocd7b9j5e',
           models: [User, Deal],
           autoLoadModels: true,
           dialectOptions: {
